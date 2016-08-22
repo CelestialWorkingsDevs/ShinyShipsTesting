d71873166ec2a6669ba55ce33b33fd90bfbe0d79
WillHaddix <WillHaddix@users.noreply.github.com>
2016-08-22 13:00:19 -0700

    Innitial Commit
    The start of the stuff for the mod Shiny Ships

---
+++ cworkings-shinyships.mod
@@ -0,0 +1,11 @@
+name="Celestial Workings - Shiny Ships"
+path="mod/cworkings-shinyships"
+tags={
+	"Military"
+	"Fixes"
+	"Balance"
+	"Gameplay"
+	"Celestial Workings"
+}
+picture="celestialworkings-icon-shinyships.jpg"
+supported_version="1.2.*"
---
+++ cworkings-shinyships/common/defines/expanded_spaceport_defines.lua
@@ -0,0 +1,50 @@
+---------------
+
+-- Whiped up by the mod lead ( I change my name sometime's)
+
+-- This shall be for all military related defines stuff
+
+------- The defines start here...
+
+
+
+-- NGameplay
+NDefines.NGameplay.SPACEPORT_LEVELS = 25
+
+
+-- NEconomy
+NDefines.NEconomy.SPACEPORT_UPGRADE_COSTS	= { 300.0, 375.0, 450.0, 525.0, 600.0, 675.0, 850.0, 925.0, 1000.0, 1075.0, 1150.0, 1225.0, 1300.0, 1375.0, 1450.0, 1525.0, 1600.0, 1675.0, 1750.0, 1825.0, 1900.0, 1975.0, 2050.0, 2125.0, 2200.0, 2275.0, 2350.0, 2425.0, 2500.0 }
+
+
+-- NShip
+NDefines.NShip.ENERGY_MAINTENANCE_MUL 											= 0.008;
+NDefines.NShip.FLEET_BASE_FORMATION_SCALE										= 2.8;
+NDefines.NShip.MILITARY_POWER_ESTIMATED_ARMOR_PENETRATION						= 0.5;	
+NDefines.NShip.MILITARY_POWER_SCALE												= 3.0;
+NDefines.NShip.MILITARY_POWER_HEALTH_WEIGHT										= 0.10;
+NDefines.NShip.MILITARY_POWER_DAMAGE_WEIGHT										= 0.10;
+NDefines.NShip.MILITARY_POWER_EXPONENT											= 0.75;	
+NDefines.NShip.MILITARY_POWER_EXPONENT 											= 0.51;
+NDefines.NShip.MILITARY_POWER_SPACEPORT_ADD 									= 0;
+NDefines.NShip.MILITARY_POWER_SPACEPORT_MULT 									= 1.0;
+
+
+-- NGraphics
+NDefines.NGraphics.MISSED_BEAM_LENGTH 											= 500.0;
+NDefines.NGraphics.SHIP_RANDOM_HEIGHT_OFFSET 									= 40.0;
+NDefines.NGraphics.MISSILE_HEIGHT_OFFSET 										= 27.5;
+NDefines.NGraphics.MISSILE_RANDOM_OFFSET_MIN_RADIUS 							= 0;
+NDefines.NGraphics.MISSILE_RANDOM_OFFSET_MAX_RADIUS 							= 0;
+NDefines.NGraphics.TRAILS_MISSILE_ALPHA_FADE 									= 3;
+NDefines.NGraphics.TRAILS_MISSILE_BASE_WIDTH 									= 2.0;
+NDefines.NGraphics.BALLISTIC_PROJECTILE_MISSED_LIFETIME 						= 5.0;
+
+
+-- NCombat
+NDefines.NCombat.COMBAT_BACK_OFF_RATE 											= 0.6;
+NDefines.NCombat.COMBAT_DETECT_RANGE_MULT 										= 0.75;
+NDefines.NCombat.FLEET_MOVE_MAX_DIST_SHIPS 										= 150.0;
+NDefines.NCombat.COMBAT_BASE_DAYS_UNTIL_EMERGENCY_FTL 							= 5.0;
+
+
+-- The end of the defines for ships
